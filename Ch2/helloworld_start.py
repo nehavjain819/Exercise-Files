0d49f1b90ab39ee1a8a183cbed7c71efd40aff82
neha jain <nehavjain819@gmail.com>
2020-12-29 12:39:22 -0600

    python variable declaration

--- Ch2/helloworld_start.py
+++ Ch2/helloworld_start.py
@@ -1,3 +1,9 @@
 #
 # Example file for HelloWorld
 #
+def main():
+    print("Hello World")
+    name = input("enter your name:")
+    print("nice to meet you, ",name)
+if __name__ == "__main__":
+    main()
--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -3,17 +3,22 @@
 #
 
 # Declare a variable and initialize it
-
+f=0
+print(f)
 
 
 # re-declaring the variable works
-
+f="neha"
+print(f)
 
 
 # ERROR: variables of different types cannot be combined
-
-
-
+# print("neha " + 123)
 # Global vs. local variables in functions
+def fun():
+    f="somevalue"
+    print(f)
 
+fun()
+print(f)
 
