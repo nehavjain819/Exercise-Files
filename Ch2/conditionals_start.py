603691ef4868c90bfd4930d4b783001aeba88cd4
neha jain <nehavjain819@gmail.com>
2020-12-30 13:19:52 -0600

    conditions in python

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -4,12 +4,19 @@
 
 
 def main():
-    x, y = 10, 100
+    x, y = 1000, 100
 
     # conditional flow uses if, elif, else
-
+    if(x<y):
+        st = "x is less than y"
+    elif(x==y):
+        st = "x is same as y"
+    else:
+        st = "x is greater than y"
+    print(st)
     # conditional statements let you use "a if C else b"
-
+    st = "x is less than y" if(x<y) else "x is greater than y or as equal to y"
+    print(st)
 
 if __name__ == "__main__":
     main()
