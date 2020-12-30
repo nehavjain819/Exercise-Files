f882a44d46563d83d6f73211411e03b78b7a76cf
neha jain <nehavjain819@gmail.com>
2020-12-29 22:55:37 -0600

    functions in python

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -3,17 +3,37 @@
 #
 
 # define a basic function
-
+def fun():
+    print("I am a function")
 
 # function that takes arguments
-
+def fun2(arg1,arg2):
+    print(arg1, " " , arg2)
 
 # function that returns a value
-
+def cube(x):
+    result = x*x*x
+    return result
 
 # function with default value for an argument
-
+def power(num,p=1):
+    result = 1
+    for i in range(p):
+        result = result * num
+    return result
 
 #function with variable number of arguments
+def mul_add(*arg):
+    result = 0
+    for i in arg:
+        result = result + i
+    return result
+# fun()
+# print(fun())
+# print(fun)
 
-
+print(cube(4))
+print(power(2))
+print(power(2,3))
+print(power(p=3,num=4))
+print(mul_add(2,3,4,5))
